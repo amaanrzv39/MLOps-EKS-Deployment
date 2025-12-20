f84c2782b355d0ba9ff68affba3a7b373fc2d7a3
amaanrzv39 <amaan.rizvi39@gmail.com>
2025-12-21 03:40:14 +0530

    EKS deployment

--- app/main.py
+++ app/main.py
@@ -156,4 +156,4 @@ def metrics():
 
 if __name__ == "__main__":
     # app.run(debug=True) # for local use
-    app.run(debug=True, host="0.0.0.0", port=8000)  # Accessible from outside Docker
+    app.run(debug=True, host="0.0.0.0", port=5000)  # Accessible from outside Docker
